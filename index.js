e1c20d84aededc122816c80b8d60b2981c01f1af
Rajdeep <89543670+rrajdeep@users.noreply.github.com>
2022-09-07 17:12:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,23 @@ window.onload = function () {
     var obj;
     document.getElementsByTagName("button")[0].onclick = function () {
         city = document.getElementById("search-input").value;
-
+        // Craeting openweather URL:
         var searchURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=a81010a9902afef9144e4917c8ea5eef";
         console.log(searchURL);
 
+        // xmlHttpRequest:
         var xhttp = new XMLHttpRequest;
         xhttp.onload = function () {
             const xmlDoc = xhttp.responseText;
-            console.log(xmlDoc);
             obj = JSON.parse(xmlDoc);
-            console.log(obj);
             displayWeather(obj);
         }
-
         xhttp.open("GET", searchURL, true)
         xhttp.send();
-
     }
-
+    // Deisplay weather data in weather box:
     function displayWeather(obj) {
-        let weatherBox = document.getElementsByClassName("city");
+        let weatherBox = document.getElementsByClassName("weather");
         let imgUrl = "https://openweathermap.org/img/wn/" + obj.weather[0].icon + ".png";
         weatherBox[0].innerHTML = "<h2>" + obj.name + "</h2>" +
             "<img src=" + imgUrl + " " + "alt='weather-image'>" +
