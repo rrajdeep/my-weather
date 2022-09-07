526b48774505828db035461bd3b173460e401934
Rajdeep <89543670+rrajdeep@users.noreply.github.com>
2022-09-07 21:36:44 +0530

    Clear search input value

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 window.onload = function () {
-    var city;
-    var obj;
+    var weatherObj;
     document.getElementsByTagName("button")[0].onclick = function () {
         city = document.getElementById("search-input").value;
         // Craeting openweather URL:
         var searchURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=a81010a9902afef9144e4917c8ea5eef";
-        console.log(searchURL);
 
         // xmlHttpRequest:
         var xhttp = new XMLHttpRequest;
         xhttp.onload = function () {
             const xmlDoc = xhttp.responseText;
-            obj = JSON.parse(xmlDoc);
-            displayWeather(obj);
+            weatherObj = JSON.parse(xmlDoc);
+            displayWeather(weatherObj);
+            // clear search input value:
+            document.getElementById("search-input").value = "";
         }
-        xhttp.open("GET", searchURL, true)
+        xhttp.open("GET", searchURL, true);
         xhttp.send();
     }
     // Deisplay weather data in weather box:
     function displayWeather(obj) {
         let weatherBox = document.getElementsByClassName("weather");
         let imgUrl = "https://openweathermap.org/img/wn/" + obj.weather[0].icon + ".png";
-        
+
         // Capitalize first letter of weather description:
         let weatherDescription = obj.weather[0].description;
         weatherDescription = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
+
         weatherBox[0].innerHTML = "<h2>" + obj.name + "</h2>" +
             "<img src=" + imgUrl + " " + "alt='weather-image'>" +
             "<p clas='weatherType'>" + weatherDescription + "</p>" +
@@ -32,6 +33,5 @@ window.onload = function () {
             "<p> Feels like " + obj.main.feels_like + "°</p>" +
             "<p>" + "Humidity: " + obj.main.humidity + "%" + "</p>" +
             "<p>" + "Wind Speed: " + obj.wind.speed + "MPH" + "</p>";
-        city.value = " ";
     }
 }
